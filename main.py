054a127b06fe9ae31b1adca9b5c388465959de90
Santas7 <andreypanyshkin03@gmail.com>
2023-03-14 21:27:40 +0400

    performed item b) test for the same consecutive bits

--- main.py
+++ main.py
@@ -27,14 +27,29 @@ def frequency_beat_test(sequence: str) -> dict:
     return counter
 
 
+def test_consecutive_bits(sequence) -> bool:
+    # check if there are two consecutive bits with the same value
+    for i in range(len(sequence) - 1):
+        if sequence[i] == sequence[i + 1]:
+            return True
+    # if there are no two consecutive bits with the same value, the test is successful
+    return False
+
+
 if __name__ == '__main__':
     # part a)
-    dictionary_count = frequency_beat_test(read_file("sequence.txt"))
+    sequence = read_file("sequence.txt")
+    dictionary_count = frequency_beat_test(sequence)
     frequency_zeros = dictionary_count['0']/BIT
     frequency_ones = dictionary_count['1']/BIT
     difference_the_received_frequencies = frequency_zeros - frequency_ones
-    print(difference_the_received_frequencies) # 0.0625
+    print(difference_the_received_frequencies) # 0.1328125
     # Considering that the difference between the frequencies is not significantly
     # large, so the test passed
 
     # part b)
+    print(test_consecutive_bits(sequence))
+    # True
+
+    # part c)
+    
