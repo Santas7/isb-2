e2172c667e9d1f80aec0b86e87047fc7c1963ad7
Santas7 <andreypanyshkin03@gmail.com>
2023-03-14 20:51:34 +0400

    performed a) obtained a dictionary of the number of zeros and ones, then calculated the frequencies and compared the results

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import math
 
+BIT = 128 # total number of bits
+
 
 def read_file(filename: str) -> str:
     """
@@ -12,4 +14,27 @@ def read_file(filename: str) -> str:
     return result
 
 
-print(read_file("sequence.txt"))
+def frequency_beat_test(sequence: str) -> dict:
+    counter = {
+        '0': 0,
+        '1': 0
+    }
+    for item in sequence:
+        if item == '0':
+            counter['0'] += 1
+        elif item == '1':
+            counter['1'] += 1
+    return counter
+
+
+if __name__ == '__main__':
+    # part a)
+    dictionary_count = frequency_beat_test(read_file("sequence.txt"))
+    frequency_zeros = dictionary_count['0']/BIT
+    frequency_ones = dictionary_count['1']/BIT
+    difference_the_received_frequencies = frequency_zeros - frequency_ones
+    print(difference_the_received_frequencies) # 0.0625
+    # Considering that the difference between the frequencies is not significantly
+    # large, so the test passed
+
+    # part b)
